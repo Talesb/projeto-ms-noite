25524b2ad3edd8fcd84bb1ff92c89c0fc71755a4
talesb <talesbatistafig@hotmail.com>
2024-09-24 14:00:59 -0300

    Implementando documentação

--- carro/src/main/java/com/acme/carro/controller/CarroController.java
+++ carro/src/main/java/com/acme/carro/controller/CarroController.java
@@ -3,6 +3,8 @@ package com.acme.carro.controller;
 import com.acme.carro.model.*;
 import com.acme.carro.payload.*;
 import com.acme.carro.service.*;
+import io.swagger.v3.oas.annotations.*;
+import io.swagger.v3.oas.annotations.tags.*;
 import lombok.*;
 import lombok.extern.slf4j.*;
 import org.springframework.http.*;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
+@Tag(name = "Carro Controller", description = "Operações permitidas com Carros")
 @RestController
 @RequestMapping("/")
 @RequiredArgsConstructor
@@ -19,11 +22,13 @@ public class CarroController {
     private final CarroService carroService;
     private final RevisaoService revisaoService;
 
+    @Operation(summary = "Obter todos os carros disponíveis", description = "Obtém os carros disponíveis no sistema")
     @GetMapping
     public ResponseEntity<?> findAll () {
         return ResponseEntity.ok(carroService.findAll());
     }
 
+    @Operation(summary = "Obter carro pelo ID", description = "Obtém o carro do sistema")
     @GetMapping("/{id}")
     public ResponseEntity<?> findById (@PathVariable Long id) {
         log.info("Find Carro by ID {}", id);
@@ -35,6 +40,7 @@ public class CarroController {
         }
     }
 
+    @Operation(summary = "Obter carro pelo ID", description = "\"Obtém o carro do sistema buscando pelo ID tais quais suas revisões, caso existam")
     @GetMapping("/{id}/completo")
     public ResponseEntity<?> findByIdComRevisoes (@PathVariable Long id) {
         log.info("Find Carro by ID - Completo {}", id);
@@ -43,25 +49,28 @@ public class CarroController {
 
         if (optCarro.isPresent()) {
             List<Revisao> allById = revisaoService.getAllById(id);
-            ResponsePayload responsePayload = new ResponsePayload(optCarro.get(),allById);
+            ResponsePayload responsePayload = new ResponsePayload(optCarro.get(), allById);
             return ResponseEntity.ok(responsePayload);
-        }else{
+        } else {
             return ResponseEntity.notFound().build();
         }
     }
 
+    @Operation(summary = "Deletar o carro pelo ID", description = "Deleta o carro pelo ID")
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete (@PathVariable Long id) {
         carroService.deleteById(id);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "Criar um novo carro no sistema", description = "Cria um novo carro no sistema")
     @PostMapping
     public ResponseEntity<?> save (@RequestBody Carro carro) {
         Carro saved = carroService.create(carro);
         return ResponseEntity.ok(saved);
     }
 
+    @Operation(summary = "Atualizar carro já presente no sistema", description = "Atualiza um carro já presente no sistema")
     @PutMapping
     public ResponseEntity<?> update (@RequestBody Carro carro) {
         carroService.update(carro);
